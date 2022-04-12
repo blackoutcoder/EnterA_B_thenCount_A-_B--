58e65b494c245586eb05dd18aa23e7a9fce93c94
blackoutcoder <94862044+blackoutcoder@users.noreply.github.com>
2022-04-12 16:07:55 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EnterA_B_thenCount_A___B__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Modifyer modifyer = new Modifyer();
+            modifyer.Reader();
+            modifyer.Increase();
+            modifyer.Decrease();
+        }
+
+        public class Modifyer
+        {
+            int a, b;
+
+            public void Reader()
+            {
+                Console.WriteLine("Enter A and press Enter...");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter A and press Enter...");
+                b = Convert.ToInt32(Console.ReadLine());
+            }
+            public void Increase()
+            {
+                a++;
+                Console.WriteLine(a);
+            }
+            public void Decrease()
+            {
+                b--;
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
